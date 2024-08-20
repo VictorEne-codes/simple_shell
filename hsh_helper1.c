6016c4ebfdd69cf0b1a7772e10eb7c1f2ea0bb92
lilprof01 <adewumime@gmail.com>
2024-08-20 04:04:06 -0700

    created first helper functions for the hsh

---
+++ hsh_helper1.c
@@ -0,0 +1,113 @@
+#include "shell.h"
+
+/**
+*find_builtin_cmd - entry point looks for a builtin command
+*@data: the parameter & return data struct
+*
+*Return: -1 if builtin not found, or 0 if builtin was executed successfully
+*or 1 if builtin found but not successful, or -2 if builtin signals exit()
+*/
+int find_builtin_cmd(data_t *data)
+{
+	int i, built_in_ret = -1;
+	builtin_table builtintbl[] = {
+		{"exit", _myexit},
+		{"env", _myenv},
+		{"help", _myhelp},
+		{"history", _myhistory},
+		{"setenv", _mysetenv},
+		{"unsetenv", _myunsetenv},
+		{"cd", _mycd},
+		{"alias", _myalias},
+		{NULL, NULL}
+	};
+
+	for (i = 0; builtintbl[i].type; i++)
+		if (_strcmp(data->argv[0], builtintbl[i].type) == 0)
+		{
+			data->line_count++;
+			built_in_ret = builtintbl[i].func(data);
+			break;
+		}
+	return (built_in_ret);
+}
+
+/**
+*find_cmd - entry point finds a command in PATH
+*@data: is the parameter & return data struct
+*
+*Return: void
+*/
+void find_cmd(data_t *data)
+{
+	char *path = NULL;
+	int i, k;
+
+	data->path = data->argv[0];
+	if (data->linecount_flag == 1)
+	{
+		data->line_count++;
+		data->linecount_flag = 0;
+	}
+	for (i = 0, k = 0; data->arg[i]; i++)
+		if (!is_delim(data->arg[i], " \t\n"))
+			k++;
+	if (!k)
+		return;
+
+	path = find_path(data, _getenv(data, "PATH="), data->argv[0]);
+	if (path)
+	{
+		data->path = path;
+		fork_cmd(data);
+	}
+	else
+	{
+		if ((interactive(data) || _getenv(data, "PATH=")
+			|| data->argv[0][0] == '/') && is_cmd(data, data->argv[0]))
+			fork_cmd(data);
+		else if (*(data->arg) != '\n')
+		{
+			data->status = 127;
+			print_error(data, "not found\n");
+		}
+	}
+}
+
+/**
+*fork_cmd - entry point forks an exec thread to run cmd
+*@data: is the parameter & return data struct
+*
+*Return: void
+*/
+void fork_cmd(data_t *data)
+{
+	pid_t child_pid;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error:");
+		return;
+	}
+	if (child_pid == 0)
+	{
+		if (execve(data->path, data->argv, get_environ(data)) == -1)
+		{
+			free_data(data, 1);
+			if (errno == EACCES)
+				exit(126);
+			exit(1);
+		}
+	}
+	else
+	{
+		wait(&(data->status));
+		if (WIFEXITED(data->status))
+		{
+			data->status = WEXITSTATUS(data->status);
+			if (data->status == 126)
+				print_error(data, "Permission denied\n");
+		}
+	}
+}
