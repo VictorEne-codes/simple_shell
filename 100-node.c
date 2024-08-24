b768da7343e763cf87b760e15062bcb9abe06334
VictorEne-Codes <omalevicene@gmail.com>
2024-08-24 15:02:57 -0700

    working on nodes

---
+++ 100-node.c
@@ -0,0 +1,146 @@
+#include "main.h"
+
+/**
+ * add_node - add nodes to linked list
+ * @head: input
+ * @s: input
+ * @n: input
+ *
+ * Return: list_t
+ */
+
+list_t *add_node(list_t **head, const char *s, int n)
+{
+	list_t *new_h;
+
+	if (!head)
+		return (NULL);
+	new_h = malloc(sizeof(list_t));
+	if (!new_h)
+		return (NULL);
+	_memset((void *)new_h, 0, sizeof(list_t));
+	new_h->n = n;
+	if (s)
+	{
+		new_h->s = _strdup(s);
+		if (!new_h->s)
+		{
+			free(new_h);
+			return (NULL);
+		}
+	}
+	new_h->next = *head;
+	*head = new_h;
+	return (new_h);
+}
+
+/**
+ * get_node_index - gets nodes at index
+ * @head: inpur
+ * @n: input
+ *
+ * Return: size_t
+ */
+
+size_t get_node_index(list_t *head, list_t *n)
+{
+	size_t i = 0;
+
+	while (head)
+	{
+		if (head == n)
+			return (i);
+		head = head->next;
+		i++;
+	}
+	return (-1);
+}
+
+/**
+ * delete_node_at_index - delets a node at index
+ * @head: input
+ * @ind: input
+ *
+ * Return: int
+ */
+
+int delete_node_at_index(list_t **head, unsigned int ind)
+{
+	list_t *n, *prev_n;
+	unsigned int i = 0;
+
+	if (!head || !*head)
+		return (0);
+
+	if (!ind)
+	{
+		n = *head;
+		*head = (*head)->next;
+		free(n->str);
+		free(n);
+		return (1);
+	}
+	n = *head;
+	while (n)
+	{
+		if (i == ind)
+		{
+			prev_n->next = n->next;
+			free(n->str);
+			free(n);
+			return (1);
+		}
+		i++;
+		prev_n = n;
+		n = n->next;
+	}
+	return (0);
+}
+
+/**
+ * _strdup - duplicates strings
+ * @s: input
+ *
+ * Return: char
+ */
+
+char *_strdup(const char *s)
+{
+	int len = 0;
+	char *ret;
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	while (*s++)
+	{
+		len++;
+	}
+	ret = malloc(sizeof(char) * (len + 1));
+	if (!ret)
+		return (NULL);
+	for (len++; len--;)
+	{
+		ret[len] = *--s;
+	}
+	return (ret);
+}
+
+/**
+ * _memset - sets a memory
+ * @s: input
+ * @b: input
+ * @n: input
+ *
+ * Return: char
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
---
+++ 101-node_helper.c
@@ -0,0 +1,130 @@
+#include "main.h"
+
+/**
+ * ffree - frees a memory set
+ * @ptr: input
+ *
+ * Return: void
+ */
+
+void ffree(char **ptr)
+{
+	char **a = ptr;
+
+	if (!ptr)
+		return;
+	while (*ptr)
+		free(*ptr++);
+	free(a);
+}
+
+/**
+ * add_node_end - adds a node at the end
+ * @head: input
+ * @s: input
+ * @n: input
+ *
+ * Return: list_t
+ */
+
+list_t *add_node_end(list_t **head, const char *s, int n)
+{
+	list_t *new_node, *node;
+
+	if (!head)
+		return (NULL);
+
+	node = *head;
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+	_memset((void *)new_node, 0, sizeof(list_t));
+	new_node->n = n;
+	if (s)
+	{
+		new_node->s = _strdup(s);
+		if (!new_node->s)
+		{
+			free(new_node);
+			return (NULL);
+		}
+	}
+	if (node)
+	{
+		while (node->next)
+			node = node->next;
+		node->next = new_node;
+	}
+	else
+		*head = new_node;
+	return (new_node);
+}
+
+/**
+ * _putsfd - writes char to fd
+ * @s: input
+ * @fd: input
+ *
+ * Return: int
+ */
+
+int _putsfd(char *s, int fd)
+{
+	int i = 0;
+
+	if (!s)
+	{
+		return (0);
+	}
+	while (*s)
+	{
+		i += _putfd(*s++, fd);
+	}
+	return (i);
+}
+
+/**
+ * node_starts_with - beginning of a node
+ * @node: input
+ * @p: input
+ * @c: input
+ *
+ * Return: list_t
+ */
+
+list_t *node_starts_with(list_t *node, char *p, char c)
+{
+	char *ptr = NULL;
+
+	while (node)
+	{
+		ptr = starts_with(node->str, p);
+		if (ptr && ((c == -1) || (*ptr == c)))
+			return (node);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * _putfd - pus char in fd
+ * @c: input
+ * @fd: input
+ *
+ * Return: int
+ */
+
+int _putfd(char c, int fd)
+{
+	static int n;
+	static char buffer[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || n >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, n);
+		n = 0;
+	}
+	if (c != BUF_FLUSH)
+		buffer[n++] = c;
+	return (1);
+}
