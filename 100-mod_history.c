ac40f66f0bd0fac93b821cd00d1d12db7047a38e
lilprof01 <adewumime@gmail.com>
2024-08-24 15:15:35 -0700

    still working on it

--- 100-mod_history.c
+++ 100-mod_history.c
@@ -78,3 +78,24 @@ int build_history_list(data_t *data, char *buf, int linecount)
 	}
 	return (0);
 }
+
+/**
+*_strcpy - entry point copies a string from a source to a destination
+*@dest: is the string destination
+*@src: is the string source
+*Return: pointer to destination
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
